0e3a4c5dd63b80bf6584fe02be1003da14cb0cf7
csergey8 <c.sergey@protonmail.com>
2018-09-25 18:05:32 +0300

    changes for pass lighthouse audit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default (props) => {
   return (
     <div className="pa3 ba b--green bg-lightest-blue">
+      <label for="search"></label>
       <input
+      id="search"
+      aria-label="Search Robots"
       onChange={props.searchChange} 
       type="search" 
       placeholder="Enter Robot Name" />
